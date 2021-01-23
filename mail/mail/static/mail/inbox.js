dffd0fc70cd85ee891c686d7f38796de831d63bd
Jack Aitken <jaitk11@gmail.com>
2021-01-23 14:33:31 -0500

    added compose email, but bug with request method

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -16,6 +16,26 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+  document.querySelector('#compose-form').onsubmit = function() {
+    const recipients = document.querySelector('#compose-recipients').value;
+    const subject = document.querySelector('#compose_subject').value;
+    const body = document.querySelector('#compose-body').value;
+
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+    });
+    return false;
+  }
+
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
@@ -30,4 +50,6 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  document.querySelector('#(mailbox)')
 }
