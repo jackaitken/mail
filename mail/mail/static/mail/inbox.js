4ac07795ac514fadc35375b7352e873dd02aa9bb
Jack Aitken <jaitk11@gmail.com>
2021-01-23 15:24:02 -0500

    last commit had bug. "w" added by mistake

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-wdocument.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
